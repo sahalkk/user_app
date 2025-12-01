fd52a742f5046a1436b4a655e1a13bc000c4980b
sahalkk <msahalkk357@gmail.com>
2025-12-01 11:12:00 +0530

    Implement authentication bloc, user repository integration, and initial app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:rider_app/app.dart';
+import 'package:rider_app/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
-  runApp(const MainApp());
+  runApp(MyApp(FirebaseUserRepo()));
 }
--- packages/user_repository/lib/user_repository.dart
+++ packages/user_repository/lib/user_repository.dart
@@ -4,3 +4,4 @@ export 'src/models/models.dart';
 export 'src/entities/entities.dart';
 export 'src/user_repo.dart';
 export 'user_repository.dart';
+export 'src/firebase_user_repo.dart';
