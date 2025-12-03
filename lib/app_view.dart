f598b4569c16c6bb3c8baed97886dd05745a8e76
sahalkk <msahalkk357@gmail.com>
2025-12-03 14:31:04 +0530

    Implement authentication flow with SignIn and SignUp blocs, and update app view to handle user authentication state

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,10 +1,32 @@
+import 'package:app123/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:app123/screens/auth/views/welcome_screen.dart';
+import 'package:app123/screens/home/views/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Rider App');
+    return MaterialApp(
+        title: 'Rider App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            colorScheme: ColorScheme.light(
+          surface: Colors.grey.shade100,
+          onSurface: Colors.black,
+          primary: Colors.green,
+          onPrimary: Colors.white,
+        )),
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: ((context, state) {
+            if (state.status == AuthenticationStatus.authenticated) {
+              return HomeScreen();
+            } else {
+              return WelcomeScreen();
+            }
+          }),
+        ));
   }
 }
---
+++ lib/screens/auth/views/sign_in_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final passwordController = TextEditingController();
+  final emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool signInRequired = false;
+  IconData iconPassword = CupertinoIcons.eye_fill;
+  bool obscurePassword = true;
+  String? _errorMsg;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<SignInBloc, SignInState>(listener: (context, state) {
+      if (state is SignInSuccess) {
+        setState(() {
+          signInRequired = false;
+        });
+      } else if (state is SignInFailure) {
+        setState(() {
+          signInRequired = true;
+        });
+      }
+    });
+  }
+}
