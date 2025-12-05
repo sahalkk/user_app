0b69fbdf5bc2bbc462c68f4120c2946b019e1eca
sahalkk <msahalkk357@gmail.com>
2025-12-05 13:21:45 +0530

    Add MyTextField component for reusable text input, update SignIn and SignUp screens to utilize it, and rename SignInLoading state to SignInProcess for clarity

--- lib/screens/auth/blocs/bloc/sign_in_state.dart
+++ lib/screens/auth/blocs/bloc/sign_in_state.dart
@@ -11,6 +11,6 @@ final class SignInInitial extends SignInState {}
 
 final class SignInFailure extends SignInState {}
 
-final class SignInLoading extends SignInState {}
+final class SignInProcess extends SignInState {}
 
 final class SignInSuccess extends SignInState {}
